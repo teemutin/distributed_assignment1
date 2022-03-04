dc43b41e6bc3e6b9f6b3ffd5b15341b49a923e7e
Teemu <Teemu.eloranta@student.lut.fi>
2022-03-04 08:41:01 +0200

    toka

--- start.py
+++ start.py
@@ -4,6 +4,6 @@ def mainprogram() :
     s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     s.bind((socket.gethostname(),1234))
     s.listen(5)
-    print("Terkkuja!")
+    print("Terkkuja!asdasd")
 
 mainprogram()
