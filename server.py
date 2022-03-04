c6311870e1758df168a1a4400d0c91c3914c6146
Teemu <Teemu.eloranta@student.lut.fi>
2022-03-04 14:49:42 +0200

    start

--- server.py
+++ server.py
@@ -1,9 +1,61 @@
 import socket
+import threading
 
-def mainprogram() :
-    s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    s.bind((socket.gethostname(),1234))
-    s.listen(5)
-    print("Terkkuja!asdasd")
 
-mainprogram()
+host = '127.0.0.1'
+port = 12345
+
+
+usernames = []
+clients = []
+
+
+#Start the server
+server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+server.bind((host, port))
+server.listen()
+
+# Sending Messages To All Connected Clients
+def broadcast(message):
+    for client in clients:
+        client.send(message)
+
+# Handling Messages From Clients
+def handle(client):
+    while True:
+        try:
+            # Broadcasting Messages
+            message = client.recv(1024)
+            broadcast(message)
+        except:
+            # Removing And Closing Clients
+            index = clients.index(client)
+            clients.remove(client)
+            client.close()
+            username = usernames[index]
+            broadcast('{} left!'.format(username).encode('ascii'))
+            usernames.remove(username)
+            break
+
+# Receiving / Listening Function
+def receive():
+    while True:
+        # Accept Connection
+        client, address = server.accept()
+        print("Connected with {}".format(str(address)))
+
+        # Request And Store Nickname
+        client.send('NICK'.encode('ascii'))
+        username = client.recv(1024).decode('ascii')
+        usernames.append(username)
+        clients.append(client)
+
+        # Print And Broadcast Nickname
+        print("Nickname is {}".format(username))
+        broadcast("{} joined!".format(username).encode('ascii'))
+        client.send('Connected to server!'.encode('ascii'))
+
+        # Start Handling Thread For Client
+        thread = threading.Thread(target=handle, args=(client,))
+        thread.start()
+receive()
