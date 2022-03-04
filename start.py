ffeacaf66a435884f9607700acd47519cdd7db00
Teemu <Teemu.eloranta@student.lut.fi>
2022-03-04 08:36:11 +0200

    Eka

---
+++ start.py
@@ -0,0 +1,9 @@
+import socket
+
+def mainprogram() :
+    s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    s.bind((socket.gethostname(),1234))
+    s.listen(5)
+    print("Terkkuja!")
+
+mainprogram()
